49438ee1750a2310308c4df7e84b826eb3f6d107
Danish <83973204+noone-21@users.noreply.github.com>
2022-08-15 19:40:40 +0500

    Updates

---
+++ precache-manifest.e90f483d6a37fca037e3a6fdabc28427.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b27c43d8b756ec29716",
+    "url": "/DaffodilNews/static/css/main.6d103c31.chunk.css"
+  },
+  {
+    "revision": "3b27c43d8b756ec29716",
+    "url": "/DaffodilNews/static/js/main.3b27c43d.chunk.js"
+  },
+  {
+    "revision": "ac5ed664b6e68dfe6a3b",
+    "url": "/DaffodilNews/static/js/1.ac5ed664.chunk.js"
+  },
+  {
+    "revision": "d6afed5bfbe9fd1bfcac",
+    "url": "/DaffodilNews/static/js/runtime~main.d6afed5b.js"
+  },
+  {
+    "revision": "a5a7c2f6e25fc3190ca9dbfee5f7447b",
+    "url": "/DaffodilNews/index.html"
+  }
+];
